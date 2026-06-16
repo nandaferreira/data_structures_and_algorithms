b9735a7c83bdecf301c8c55f8b4281bfc1a8d288
nandaferreira <ferreirafernanda80@yahoo.com.br>
2026-06-16 11:12:17 -0300

    added local changes in aed2's files

---
+++ pratica_semana04/01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No{
+    int destino;
+    int peso;
+    struct No *proximo;
+}No;
+
+typedef struct{
+    int V;
+    No **lista;
+}Grafo;
+
+//exercicio - complete a função
+
+void adicionarAresta(Grafo *g, int origem, int destino, int peso){
+
+    No *novo = malloc(sizeof(No));
+    novo ->destino = destino;
+    novo ->peso = peso;
+    novo ->proximo =;
+}
+
+//complexidade: O(n)
