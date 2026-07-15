773d20dc68f4aa64fd1f940a382b404f24424492
nandaferreira <ferreirafernanda80@yahoo.com.br>
2026-07-15 14:50:37 -0300

    binaryTree functions: isEmpty_BinTree; totalNodes_BinTree;

--- praticas/pratica_04_arvore_binaria/ArvBin.c
+++ praticas/pratica_04_arvore_binaria/ArvBin.c
@@ -19,23 +19,22 @@ ArvBin* cria_ArvBin(){
     return raiz;
 }
 
-//libera arvore
-/* para liberar uma árvore binária é necessária uma função para percorrer recursivamente
-todos os nós da árvore e liberá-los, libera_NO, e outra para inicializar a destruição da árvore e liberar
-a memória alocada para a raiz, libera_ArvBin*/
-
+//libera no
+/* para liberar uma árvore binária é necessária uma função para percorrer recursivamente todos os nós da árvore e liberá-los, libera_NO, 
+e outra para inicializar a destruição da árvore e liberar a memória alocada para a raiz, libera_ArvBin*/
 
 void libera_NO(struct NO* no){
     if (no == NULL){
         return;
     }else{
-        libera_NO(no->esq);
-        libera_NO(no->dir);
-        free(no);
+        libera_NO(no->esq); //acessa o NO esquerdo
+        libera_NO(no->dir); //acessa o NO direito
+        free(no); //libera o No
         no = NULL;
     }
 }
 
+//libera arvore
 void libera_ArvBin(ArvBin* raiz){
     if(raiz == NULL){
         return;
@@ -46,8 +45,34 @@ void libera_ArvBin(ArvBin* raiz){
 }
 
 
-
 //estaVazia
+// Uma ávore binária será considerada vazia sempre que o conteúdo da sua 'raiz' apontar para a constante NULL
+
+int estaVazia_ArvBin(ArvBin* raiz){
+    if(raiz == NULL){ //verifica primeiro se o ponteiro ArvBin* raiz é igual a NUll. Se for verdade, teve algum problema na criação da árvore e, nesse caso, não teria uma árvore válida para trabalhar
+        return 1; //erro de criacao - arvore invalida
+    }else if(*raiz == NULL){ //se foi criada com sucesso, entao é possivel acessar o conteudo da sua 'raiz' (*raiz) e comparar com NULL. Se for NULL, valor atribuido a raiz quando vazia, entao a arvore esta vazia
+        return 1;}
+    return 0; // nao esta vazia
+}
+
+int totalNO_ArvBin(ArvBin* raiz){
+    //sempre verificar primeiro se a arvore foi corretamente criada, ou seja, se a raiz nao eh NULL. Se nao for, eh possivel entrar no conteudo (*raiz) e comecar a contagem
+
+    if(raiz == NULL){
+        return 0;
+    }else if(*raiz == NULL){
+        return 0;
+    }
+    int total_esq = totalNO_ArvBin(&((*raiz)-> esq));
+    int total_dir = totalNO_ArvBin(&((*raiz)-> dir));
+    return(total_esq + total_dir + 1);
+
+}
+
+
+
+
 //inserir elemento
 //remover elemento
 //busca elemento
--- praticas/pratica_04_arvore_binaria/ArvBin.h
+++ praticas/pratica_04_arvore_binaria/ArvBin.h
@@ -1,13 +1,13 @@
 typedef struct NO* ArvBin;
 
 ArvBin* cria_ArvBin();
-void libera_ArvBin(ArvBin* raiz);
-int insere_ArvBin(ArvBin* raiz, int valor);
+void libera_ArvBin(ArvBin* raiz); //apaga estrutura da arvore
+int insere_ArvBin(ArvBin* raiz, int valor); //insere elemento
 int remove_ArvBin(ArvBin* raiz, int valor);
-int estaVazia_ArvBin(ArvBin* raiz);
-int totalNO_ArvBin(ArvBin* raiz);
-int altura_ArvBin(ArvBin* raiz);
-int consulta_ArvBin(ArvBin* raiz, int valor);
+int estaVazia_ArvBin(ArvBin* raiz); //remove um elemento
+int totalNO_ArvBin(ArvBin* raiz); //contagem de total de NOs da arvore
+int altura_ArvBin(ArvBin* raiz); //qual a altura
+int consulta_ArvBin(ArvBin* raiz, int valor); //consulta se um elemento X pertence a arvore
 void preOrdem_ArvBin(ArvBin* raiz);
 void emOrdem_ArvBin(ArvBin* raiz);
 void posOrdem_ArvBin(ArvBin* raiz);
