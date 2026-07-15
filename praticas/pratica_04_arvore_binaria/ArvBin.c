8cf0c280f5c95a596568aa0aabe110aa2c572d99
nandaferreira <ferreirafernanda80@yahoo.com.br>
2026-07-14 23:08:09 -0300

    basic_functions: createBinaryTree; freeNode; freeBinaryTree

--- praticas/pratica_04_arvore_binaria/ArvBin.c
+++ praticas/pratica_04_arvore_binaria/ArvBin.c
@@ -1,3 +1,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include "ArvBin.h"
+
+typedef struct NO{
+    NO* esq;
+    int dado;
+    NO* dir;
+}NO;
+
+//cria arvore
+ArvBin* cria_ArvBin(){
+    ArvBin* raiz;
+    raiz = (ArvBin*)malloc(sizeof(ArvBin));
+
+    if(raiz != NULL){
+        *raiz = NULL;
+    }
+    return raiz;
+}
+
+//libera arvore
+/* para liberar uma árvore binária é necessária uma função para percorrer recursivamente
+todos os nós da árvore e liberá-los, libera_NO, e outra para inicializar a destruição da árvore e liberar
+a memória alocada para a raiz, libera_ArvBin*/
+
+
+void libera_NO(struct NO* no){
+    if (no == NULL){
+        return;
+    }else{
+        libera_NO(no->esq);
+        libera_NO(no->dir);
+        free(no);
+        no = NULL;
+    }
+}
+
+void libera_ArvBin(ArvBin* raiz){
+    if(raiz == NULL){
+        return;
+    }else{
+        libera_NO(*raiz); //libera cada nó
+        free(raiz); //libera a raiz
+    }
+}
+
+
+
+//estaVazia
+//inserir elemento
+//remover elemento
+//busca elemento
+//calcula altura
+//preOrdem
+//emOrdem
+//posOrdem
